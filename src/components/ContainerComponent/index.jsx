808de59b5263a400674730471edaa277880ffa21
Leopoldo Gurgel <leopoldogbp@gmail.com>
2024-05-30 16:54:57 -0400

    my head is spinning at this moment

--- src/components/ContainerComponent/index.jsx
+++ src/components/ContainerComponent/index.jsx
@@ -1,8 +1,80 @@
 import DestaqueComponent from "../DestaqueComponent"
 import SideGroupComponent from "../SideGroupComponent";
+import { QUERY_BREAKING, QUERY_ENTERT, QUERY_SPORTS } from '../ultils/queries.js';
+import { UPDATE_BREAKING, UPDATE_ENTERTAINMENT, UPDATE_SPORTS } from "../utils/actions";
+import useNews from "../../../ultils/newsContext.jsx";
 
 
-const ContainerComponent = () => {    
+const ContainerComponent = () => {
+
+    const [state, dispatch] = useNews();
+    
+    const {
+        loading: breakingLoading, 
+        error: breakingError, 
+        data: breakingData} = useQuery(QUERY_BREAKING);
+    const {
+        loading: sportsLoading, 
+        error: sportsError, 
+        data: sportsData} = useQuery(QUERY_SPORTS);
+    const {
+        loading: entertainmentLoading, 
+        error: entertainmentError, 
+        data: entertainmentData} = useQuery(QUERY_ENTERT);
+  
+  
+        
+    if(breakingLoading || sportsLoading || entertainmentLoading) {
+        return (
+            <div>Loading...</div>
+        )
+    }
+    
+
+    if(breakingError) {
+        return (
+            <div>{breakingError.message}</div>
+        )
+    } 
+    if(sportsError){
+        return(
+            <div>{sportsError.message}</div>
+        )
+    }
+    if(entertainmentError){
+        return(
+            <div>{entertainmentError.message}</div>
+        )
+    }
+
+    useEffect(() => {
+        if(breakingData){
+            dispatch({
+                type: UPDATE_BREAKING,
+                payload: breakingData.breaking
+            })
+        }
+    }, [breakingData])
+
+    useEffect(() => {
+        if(sportsData){
+            dispatch({
+                type: UPDATE_SPORTS,
+                payload: sportsData.sports
+            })
+        }
+    }, [sportsData])
+
+    useEffect(() => {
+        if(entertainmentData){
+            dispatch({
+                type: UPDATE_ENTERTAINMENT,
+                payload: entertainmentData.entertainment
+            })
+        }
+    }, [entertainmentData])
+
+
     
     return (
         <div className="container-fluid row">
--- ultils/newsContext.jsx
+++ ultils/newsContext.jsx
@@ -22,71 +22,7 @@ export const NewsProvider = ({children}) => {
         sportsArticles
     })
 
-    const {
-        loading: breakingLoading, 
-        error: breakingError, 
-        data: breakingData} = useQuery(QUERY_BREAKING);
-    const {
-        loading: sportsLoading, 
-        error: sportsError, 
-        data: sportsData} = useQuery(QUERY_SPORTS);
-    const {
-        loading: entertainmentLoading, 
-        error: entertainmentError, 
-        data: entertainmentData} = useQuery(QUERY_ENTERT);
-
     
-    if(breakingLoading || sportsLoading || entertainmentLoading) {
-        return (
-            <div>Loading...</div>
-        )
-    }
-    
-
-    if(breakingError) {
-        return (
-            <div>{breakingError.message}</div>
-        )
-    } 
-    if(sportsError){
-        return(
-            <div>{sportsError.message}</div>
-        )
-    }
-    if(entertainmentError){
-        return(
-            <div>{entertainmentError.message}</div>
-        )
-    }
-
-    useEffect(() => {
-        if(breakingData){
-            dispatch({
-                type: UPDATE_BREAKING,
-                payload: breakingData.breaking
-            })
-        }
-    }, [breakingData])
-
-    useEffect(() => {
-        if(sportsData){
-            dispatch({
-                type: UPDATE_SPORTS,
-                payload: sportsData.sports
-            })
-        }
-    }, [sportsData])
-
-    useEffect(() => {
-        if(entertainmentData){
-            dispatch({
-                type: UPDATE_ENTERTAINMENT,
-                payload: entertainmentData.entertainment
-            })
-        }
-    }, [entertainmentData])
-
-
     return(
         <NewsContext.Provider value={[state, dispatch]}>
             {children}
