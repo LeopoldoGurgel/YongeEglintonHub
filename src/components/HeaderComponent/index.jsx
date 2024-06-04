caf0c2a55a520efb958d3f046f14e29addf25754
Leopoldo Gurgel <leopoldogbp@gmail.com>
2024-06-04 13:43:27 -0400

    trying to fix a little bug

--- src/components/HeaderComponent/index.jsx
+++ src/components/HeaderComponent/index.jsx
@@ -1,7 +1,6 @@
 import './header.css'
 import React from 'react';
 import {Link} from 'react-router-dom';
-import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
